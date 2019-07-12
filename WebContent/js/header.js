001dab33f2b892e91425fe3aba0f2da2ceb416a9
user000421 <white.user.002@gmail.com>
2019-07-12 12:18:12 +0900

    途中です

--- WebContent/js/header.js
+++ WebContent/js/header.js
@@ -21,3 +21,15 @@ $(function(){
 	var height=$('header').height();
 	$('body').css('margin-top', height + 14);
 });
+
+// 検索ボックス の placeholder 操作
+$(function(){
+	$('input[type="text"]').eq(0).focus( function(){
+		$('input[type="text"]').eq(0).removeAttr('placeholder');
+	});
+	$('input[type="text"]').eq(0).blur( function(){
+		if($('input[type="text"]').eq(0).val().length == 0){
+			$('input[type="text"]').eq(0).attr('placeholder', '検索ワード');
+		}
+	});
+});
