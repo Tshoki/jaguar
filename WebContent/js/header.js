afb57d3b718eb4e867b8a59936717d81f2fcf59b
hunterkuro <hunterkurojp@gmail.com>
2019-07-18 16:17:25 +0900

    コメント消した

--- WebContent/js/header.js
+++ WebContent/js/header.js
@@ -20,18 +20,4 @@ function goSearchItemAction(){
 $(function(){
 	var height=$('header').height();
 	$('body').css('margin-top', height + 14);
-
-	// 2019 07 16 考察中 ひどい指定
-	//var h1_height=$('h1').height(); // 新変数追加
-	//$('h1').next().css('margin-top', h1_height + height + 100); // h1 の 次の要素に margin 追加
 });
-
-/*
-$(function(){
-	setTimeout(function(){
-		$('h1').text('Cart'); // 表示文字操作
-		$('h1').css('border','none'); // border 操作
-		$('h1').animate({'left':'15%'},{duration: 300}); // position 操作
-	},1000);
-});
-*/
